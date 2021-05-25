d5da35a16e55bc88b333f352ab9b5f33f1ff5427
AnnaAgeneau <anna.ageneau2021@campus-eni.fr>
2021-05-25 09:07:39 +0200

    commit de mon arche de Noe

--- src/fr/eni/ArcheDeNoe/Arche.java
+++ src/fr/eni/ArcheDeNoe/Arche.java
@@ -1,6 +1,7 @@
 package fr.eni.ArcheDeNoe;
 
-import java.util.Scanner;
+import java.util.ArrayList;
+import java.util.Iterator;
 
 /**
  * auteur anna ageneau
@@ -13,71 +14,149 @@ import java.util.Scanner;
 public class Arche {
 
     // je crée un tableau de 8 car on ne peut mettre que 8 animaux dans mon arche
-    private Animal[] tabAnimaux = new Animal[8];
+    // private Animal[] tabAnimaux = new Animal[8];
+    ArrayList<Animal> listeAnimaux = new ArrayList<Animal>();
+    public static int capaciteArche = 8;
+    /*private boolean gorilleMasculin = false;
+    private boolean gorilleFeminin = false;
+    private boolean chatMasculin = false;
+    private boolean chatFeminin = false;
+    private boolean lapinMasculin = false;
+    private boolean lapinFeminin = false;
+    private boolean chienMasculin = false;
+    private boolean chienFeminin = false;
+*/
 
     public Arche() {
     }
 
-    public void quelSexe(String quelSexe){
-        if (quelSexe.equals("M")) {
-            System.out.println("Masculin");
-        } else if (quelSexe.equals("F")) {
-            System.out.println("Feminin");
-        } else {
-            System.out.println("Veuillez écrire M ou F svp");
+    /**
+     * méthode pour ajouter un animal dans l'arche
+     * indication du sexe et de l'espèce de l'animal
+     * puis ajout de cet animal dans l'arche
+     * @param animal
+     *
+     */
+
+    public void ajouterAnimal(Animal animal) throws ExceptionArche{
+        controleIndividu(animal);
+       /* if (animal instanceof Chien) {
+            if (animal.sexe.equals("M"))
+                chienMasculin = true;
+            else chatFeminin = true;
         }
+        if (animal instanceof Chat) {
+            if (animal.sexe.equals("M"))
+                chatMasculin = true;
+            else chatFeminin = true;
+        }
+        if (animal instanceof Gorille) {
+            if (animal.sexe.equals("M"))
+                gorilleMasculin = true;
+            else gorilleFeminin = true;
+        }
+        if (animal instanceof Lapin) {
+            if (animal.sexe.equals("M"))
+                lapinMasculin = true;
+            else lapinFeminin = true;
+        }*/
+
+        listeAnimaux.add(animal);
+
     }
 
-    public void ajouterAnimal(String nom, String sexe, int espece) {
-        switch (espece) {
-            case 0:
 
-                Animal animal0 = new Chat(nom, sexe);
-                for (int i = 0; i < tabAnimaux.length; i++) {
-                    if (this.tabAnimaux[i] == null) {
-                        this.tabAnimaux[i] = animal0;
-                    }
-
-                }
-                break;
-
-            case 1:
-                Animal animal1 = new Chien(nom, sexe);
-                for (int i = 0; i < tabAnimaux.length; i++) {
-                    if (this.tabAnimaux[i] == null) {
-                        this.tabAnimaux[i] = animal1;
-                    }
-                }
-                break;
-
-            case 2:
-                Animal animal2 = new Gorille(nom, sexe);
-                for (int i = 0; i < tabAnimaux.length; i++) {
-                    if (this.tabAnimaux[i] == null) {
-                        this.tabAnimaux[i] = animal2;
-                    }
-                }
-                break;
-
-            case 3:
-                Animal animal3 = new Lapin(nom, sexe);
-                for (int i = 0; i < tabAnimaux.length; i++) {
-                    if (this.tabAnimaux[i] == null) {
-                        this.tabAnimaux[i] = animal3;
-                    }
-                    break;
-                }
-
-        }
-    }
-
-            public void capaciteArche (int capacite){
-
+    /**
+     * méthode qui regarde dans la liste au fur et à mesure l'animal + son sexe
+     *
+     *
+     * @param animalAAjouter
+     * @throws ExceptionArche
+     */
+    public void controleIndividu(Animal animalAAjouter) throws ExceptionArche {
+        Iterator<Animal> it = listeAnimaux.iterator();
+        // hasNext permet au curseur de vérifier s'il y a un élément suivant
+        while (it.hasNext()) {
+            // next permet de renvoyer le prochain élément de la liste
+            Animal animalEnCoursDeTest = it.next();
+            // si mon animal en cours de test à les memes cacteristique que l'animal que j'ajoute
+            // - meme espece (on teste la classe des 2 objets avec getClass)
+            // - meme sexe (on teste avec l'attribut sexe)fv
+            // alors je renvoie une exception
+            if (animalEnCoursDeTest.getClass().equals(animalAAjouter.getClass())
+                    && animalEnCoursDeTest.sexe.equals(animalAAjouter.sexe)) {
+                throw new ExceptionArche("Une animal de même espèce et de même sexe est déjà présent");
             }
 
 
+        }
+    }
 
-}
+
+    public boolean placeDispo() {
+        return listeAnimaux.size() < capaciteArche;
+
+    }
+    /*
+    /**
+     * métholde qui permet de relever les expections selon le sexe de l'animal
+     * il faut un animal de chaque sexe dans l'arche
+     *
+     * @param animal
+     * @throws ExceptionArche
+     */
+    /*
+    private void controleIndividu (Animal animal) throws ExceptionArche{
+        if (animal instanceof Chien){
+            if (animal.sexe.equals("M") ) {
+                if (chienMasculin) {
+                    throw new ExceptionArche("un chien mâle est déjà présent à bord ! Sorry !!");
+                }
+            } else if (chienFeminin){
+                    throw new ExceptionArche("une femelle chien est déjà à bord ! Sorry !");
+
+            }
+        }
+        if (animal instanceof Chat){
+            if (animal.sexe.equals("M") ) {
+                if (chatMasculin) {
+                    throw new ExceptionArche("un chat mâle est déjà présent à bord ! Sorry !!");
+                }
+            } else if (chatFeminin){
+                throw new ExceptionArche("une femelle chat est déjà à bord ! Sorry !");
+
+            }
+        }
+        if (animal instanceof Gorille){
+            if (animal.sexe.equals("M") ) {
+                if (gorilleMasculin) {
+                    throw new ExceptionArche("un gorille mâle est déjà présent à bord ! Sorry !!");
+                }
+            } else if (gorilleFeminin){
+                throw new ExceptionArche("une femelle gorille est déjà à bord ! Sorry !");
+
+            }
+        }
+        if (animal instanceof Lapin){
+            if (animal.sexe.equals("M") ) {
+                if (lapinMasculin) {
+                    throw new ExceptionArche("un lapin mâle est déjà présent à bord ! Sorry !!");
+                }
+            } else if (lapinFeminin){
+                throw new ExceptionArche("une femelle lapin est déjà à bord ! Sorry !");
+
+            }
+        }
+
+     */
+
+
+
+
+    }
+
+
+
 
 
 
--- src/fr/eni/ArcheDeNoe/MainArche.java
+++ src/fr/eni/ArcheDeNoe/MainArche.java
@@ -4,54 +4,103 @@ import javax.sound.midi.Soundbank;
 import java.util.Scanner;
 
 class Main {
+    public static Scanner scan = new Scanner(System.in);
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws ExceptionArche {
 
 
         Arche arche = new Arche();
-        Arche sexe = new Arche();
-        int capacite = 0;
         char reponse = 0;
         String nom;
+        String sexe;
 
+        // je crée une boucle où je peux entrer le nom, sexe et espece de mon animal, tant que
+        // la capacité de l'arche n'est pas atteinte
         do {
             System.out.println("$$$$$ WELCOME DANS L'ARCHE, je suis Noé $$$$$");
             // Noe renseigne le nom de l'animal
-            Scanner scan = new Scanner(System.in);
+
             System.out.println("Quel est ton nom ?");
             nom = scan.nextLine();
 
             // Noe renseigne le sexe de l'animal
             System.out.println("Quel est ton sexe? (M pour masculin, F féminin)");
-            String saisie = scan.nextLine();
-            sexe.quelSexe(saisie);
+            sexe = saisieSexe();
 
             // Noe renseigne de quelle espece est l'animal
             System.out.println("Quel est ton espèce ? 0 pour chat, 1 pour chien, 2 pour gorille, 3 pour lapin");
-            int saisie2 = scan.nextInt();
+            int espece = scan.nextInt();
             scan.nextLine();
-            arche.ajouterAnimal(nom, saisie, saisie2);
-            capacite++;
+
+            Animal animalAAjouter = null;
+
+            switch (espece) {
+                case 0:
+                    animalAAjouter = new Chat(nom, sexe);
+                    break;
+
+                case 1:
+                    animalAAjouter = new Chien(nom, sexe);
+                    break;
+
+                case 2:
+                    animalAAjouter = new Gorille(nom, sexe);
+                    break;
+
+                case 3:
+                    animalAAjouter = new Lapin(nom, sexe);
+                    break;
+            }
+            try {
+                arche.ajouterAnimal(animalAAjouter);
+                System.out.println("l'animal " +  animalAAjouter.nom + " est dans l'arche");
+            } catch (ExceptionArche e){
+                System.out.println(e.getMessage());
+            }
+
+
             System.out.println("Voulez-vous ajouter un animal ? (O pour continuer N pour quitter)");
 
             reponse = scan.nextLine().charAt(0);
             //
 
-        } while (reponse == 'O' || reponse == 'o');
+        } while ((reponse == 'O' || reponse == 'o') && arche.placeDispo());
 
-            if (reponse == 'N' || reponse == 'n') {
+        if (!arche.placeDispo()) {
+            System.out.println("on est complet !!");
 
-                System.out.println("l'animal " + nom + " est dans l'arche");
-                System.out.println("Vous avez " + capacite + "/8 animaux dans l'arche");
-
-                System.out.println("L'arche possède à son bord : " + Chat.getNbChat() + " chat(s), " + Chien.getNbChien() + " chien(s), " +
-                        Gorille.getNbGorille() + " gorille(s), " + Lapin.getNbLapin() + " lapin(s)");
+        }
 
 
-                System.out.println("Le commis doit prévoir à bord " + StockNourriture.stockVegetaux() + " végétaux et " + StockNourriture.stockViande() + "kg de viande");
+
+        System.out.println("Vous avez " + arche.listeAnimaux.size() + "/" + Arche.capaciteArche + " animaux dans l'arche");
+
+        System.out.println("L'arche possède à son bord : " + Chat.getNbChat() + " chat(s), " + Chien.getNbChien() + " chien(s), " +
+                Gorille.getNbGorille() + " gorille(s), " + Lapin.getNbLapin() + " lapin(s)");
 
 
+        System.out.println("Le commis doit prévoir à bord " + StockNourriture.stockVegetaux() + " végétaux et " + StockNourriture.stockViande() + "kg de viande");
+
+
+    }
+
+    public static String saisieSexe() {
+        String saisie;
+
+        do {
+            saisie = scan.nextLine().toUpperCase();
+
+            if (saisie.equals("M")) {
+                System.out.println("Masculin");
+            } else if (saisie.equals("F")) {
+                System.out.println("Feminin");
+            } else {
+                System.out.println("Veuillez écrire M ou F svp");
             }
+
+        } while (!saisie.equals("M") && !saisie.equals("F"));
+
+        return saisie;
     }
 
 }
